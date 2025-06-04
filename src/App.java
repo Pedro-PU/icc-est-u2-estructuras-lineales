85eeb5252a65ac898a964e0e4170363fb17203b3
markzuck3rberg <141587136+markzuck3rberg@users.noreply.github.com>
2025-06-04 08:42:25 -0500

    Cambios clase

--- src/App.java
+++ src/App.java
@@ -1,15 +1,49 @@
-import Controllers.Stack;
-
+import Controllers.*;
+import Models.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Stack stack = new Stack();
         stack.push(10);
         stack.push(-1);
-        stack.push(0);
         stack.push(5);
+        stack.push(8);
+        System.out.println("tam = " + stack.size());
         stack.printStack();
         stack.pop();
-        stack.peek();
+        //stack.peek();
+        System.out.println("tam = " + stack.size());
         stack.printStack();
+
+        StackG<String> stackGS = new StackG<String>();
+        stackGS.push("uno");
+        stackGS.push("cuatro");
+        System.out.println("tam = "+ stackGS.size());
+        stackGS.printStack();
+
+        Cola cola = new Cola();
+        cola.add(5);
+        cola.add(7);
+        cola.add(10);
+        System.out.println("tam = " + cola.size);
+        cola.printCola();
+        cola.remove();
+        System.out.println("tam = " + cola.size);
+        cola.printCola();
+
+        System.out.println("-".repeat(50));
+
+        Persona p1 = new Persona("Pablo");
+        Persona p2 = new Persona("Juan");
+        Persona p3 = new Persona("Maria");
+
+        ColaG<Persona> colaPersonas = new ColaG<Persona>();
+        colaPersonas.add(p1);
+        colaPersonas.add(p2);
+        colaPersonas.add(p3);
+        System.out.println(colaPersonas.size());
+        colaPersonas.printCola();
+        System.out.println("Atiene a la persona => " + colaPersonas.remove().getNombre());
+        System.out.println(colaPersonas.size());
+        colaPersonas.printCola();
     }
 }
--- src/Controllers/StackG.java
+++ src/Controllers/StackG.java
@@ -1,53 +1,48 @@
 package Controllers;
+
 import java.util.EmptyStackException;
 
-import Models.Node;
-public class Stack {
-    private Node top;
-    private int size;
+import Models.NodeGeneric;
 
-    public Stack() {
+public class StackG<T> {
+    private NodeGeneric<T> top;
+    private int size;
+    public StackG() {
         this.top = null;
         this.size = 0;
     }
 
-    //Metodo que ingrese un valor int
-    public void push(int value){
-        Node newNode = new Node(value);
+    public void push(T value){
+        NodeGeneric<T> newNode = new NodeGeneric<>(value);
         newNode.setNext(top);
         top = newNode;
         size++;
     }
-
-    public int pop(){
-        if (isEmpty()){
+    public T pop(){
+        if (isEmpty())
             throw new EmptyStackException();
-        }
-        int value = top.getValue();
+        T value = top.getValue();
         top = top.getNext();
         size--;
         return value;
     }
-
-    public int peek(){
+    public T peek(){
         if (isEmpty())
             throw new EmptyStackException();
         return top.getValue();
     }
-
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return top == null;
     }
-
+    public int size(){
+        return size;
+    }
     public void printStack(){
-        Node current = top;
+        NodeGeneric<T> current = top;
         while(current != null){
             System.out.print(current.getValue() + " | ");
             current = current.getNext();
         }
         System.out.println();
-    }
-    public int size(){
-        return size;
     }
 }
